ce5bb351b2877afdfc6cb938a6b2c4475fb73011
Beesan-Abu-Mfarreh <beesanahmad2001@gmail.com>
2022-08-09 10:29:42 +0300

    api task

---
+++ task3/Controllers/Users.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using task3.Repo;
+using task3.Mdels;
+namespace task3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Users : ControllerBase
+    {
+
+        public Users()
+        {
+
+        }
+        [HttpGet]
+        public ActionResult<List<User>> GetAll()
+        {
+            return UserRepo.GetAll();
+        }
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            var user = UserRepo.Get(id);
+            if (user == null)
+                return NotFound();
+            return user;
+        }
+        [HttpDelete]
+
+        public ActionResult Delete(int id)
+        {
+            var user = UserRepo.Get(id);
+            if (user == null)
+                return NotFound();
+            UserRepo.delete(id);
+            return Ok();
+        }
+        [HttpPost]
+        public ActionResult Creat(User user)
+        {
+            UserRepo.add(user);
+            return Ok();
+        }
+        [HttpPut]
+      public ActionResult Update(User user)
+        {
+            var _user = UserRepo.Get(user.Id);
+            if(_user == null)
+                return NotFound();
+            UserRepo.update(user);
+            return Ok();
+        }
+    }
+
+}
---
+++ task3/Repo/UserRepo.cs
@@ -0,0 +1,53 @@
+﻿using task3.Mdels;
+namespace task3.Repo
+{
+    public class UserRepo
+    {
+
+        static List<User> users { get; set; }
+        static UserRepo()
+        {
+            users = new List<User>()
+            {
+                new User() { Id = 1, Name = "Aliiiiiiiii"},
+                new User() { Id = 2, Name = "Sara" },
+                new User() { Id = 3, Name = "beesan" },
+                new User() { Id = 4, Name = "ahmad" }
+        };
+        }
+        public static List<User> GetAll()
+        {
+            return users;
+        }
+        public static User Get(int id)
+        {
+            return users.FirstOrDefault(user => user.Id == id);
+        }
+        public static void delete(int id)
+        {
+            var user = Get(id);
+            if (user != null)
+                users.Remove(user);
+        }
+
+        public static void add (User user)
+        {
+           users.Add(user);
+       
+
+
+
+        }
+        public static void update(User user)
+        {
+            var index = users.FindIndex(e=> e.Id == user.Id);
+            if (index == -1)
+                return;
+            users[index] = user;
+        }
+    }
+    }
+//
+
+
+
